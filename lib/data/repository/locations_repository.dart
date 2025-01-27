b12937c009bca03fac86d0b644db5b20c47eb861
nadiiaxd <163354973+nadiiaxd@users.noreply.github.com>
2025-01-27 15:48:03 -0600

    f
    f

--- lib/data/repository/locations_repository.dart
+++ lib/data/repository/locations_repository.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 import 'package:rick_and_morty_fase_2/data/api/locations_api.dart';
 import 'package:rick_and_morty_fase_2/data/models/location/location.dart';
 import 'package:rick_and_morty_fase_2/data/models/location/location_list.dart';
